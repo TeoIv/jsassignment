d3a86eb435081ef578072cca4d0623b45f0b0145
TeoIv <90186068+TeoIv@users.noreply.github.com>
2021-09-21 12:04:31 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
 console.log('JavaScript is working!')
+
+function drawDiamond(context, x, y, width, height) {
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x - width / 2, y + height / 2);
+    context.lineTo(x, y + height);
+    context.lineTo(x + width / 2, y + height / 2);
+    context.closePath();
+
+    context.fillStyle = "black";
+    context.fill();
+}
+window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    drawDiamond(context, 150, 40, 75, 90);
+};
